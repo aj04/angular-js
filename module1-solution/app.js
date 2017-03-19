98ae91e788dfe050f1c15047ac3d8031a27728a0
Ajay Dubey <Ajay Dubey>
2017-03-19 16:05:59 -0400

    Assignment Complete with clear indentation

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -4,20 +4,20 @@ angular.module("LunchCheck", [])
 
 .controller("LunchCheckController", LunchCheckController);
 LunchCheckController.$inject = ["$scope"];
-function LunchCheckController($scope) {
-	$scope.LunchList = "";
-	$scope.message = "";
-	$scope.CheckIfTooMuch= function(){
-		if( $scope.LunchList == "") {
-		  $scope.message = "Please enter data first";
-		  return;
-		}
-	   var temp = $scope.LunchList.split(',');
-	   if(temp.length > 0 && temp.length <= 3) {
-	   	 $scope.message = "Enjoy";
-	   } else {
-	   	 $scope.message = "Too Much";
-	   }
-	};
-};
+	function LunchCheckController($scope) {
+		$scope.LunchList = "";
+		$scope.message = "";
+		$scope.CheckIfTooMuch= function(){
+			if( $scope.LunchList == "") {
+			  $scope.message = "Please enter data first";
+			  return;
+			}
+		    var temp = $scope.LunchList.split(',');
+		    if(temp.length > 0 && temp.length <= 3) {
+		   	   $scope.message = "Enjoy";
+		    } else {
+		   	   $scope.message = "Too Much";
+		    }
+		};
+    };
 })();
